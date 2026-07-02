9ac7ce6854453dd7000e02f3dcfa153ed15b6631
Kota-Venkat1 <kotavenkat1206@gmail.com>
2026-07-02 15:01:43 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) return false;
+    HashMap<Character,Integer> map1 = new HashMap<>();
+    HashMap<Character,Integer> map2 = new HashMap<>();
+
+    for(int i=0;i<s.length();i++){
+        if(map1.containsKey(s.charAt(i))){
+            map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
+        }
+        else
+        map1.put(s.charAt(i),1);
+    }
+    for(int i=0;i<t.length();i++){
+        if(map2.containsKey(t.charAt(i))){
+            map2.put(t.charAt(i),map2.get(t.charAt(i))+1);
+        }
+        else
+        map2.put(t.charAt(i),1);
+    }
+    if(map1.size()!=map2.size())
+    return false;
+    for(Map.Entry<Character,Integer> entry : map1.entrySet()){
+        Character key = entry.getKey();
+        if(!java.util.Objects.equals(map1.get(key), map2.get(key)))
+        return false;
+    }
+    return true;
+    }
+}
